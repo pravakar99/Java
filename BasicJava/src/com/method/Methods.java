2e66d615918a610c69d7eaed61288898bbd3766e
pravakar <pravakarmuduli99@gmail.com>
2022-01-31 23:14:41 +0530

    hll

---
+++ BasicJava/src/com/method/Methods.java
@@ -0,0 +1,26 @@
+package com.method;
+
+public class Methods {
+	public static void main(String args[])
+	{
+		
+	}	
+
+	class Method2
+	{
+		int ae(int a)
+		{
+			int b=a;
+			return b;
+			
+			
+		}
+		String name(String g)
+		{
+			
+			String h=g;
+			return h;
+			
+		}
+	}
+}
---
+++ BasicJava/src/com/variable/Varibable.java
@@ -0,0 +1,16 @@
+package com.variable;
+
+public class Varibable {
+	int m=100; //instance variable
+	static int n=70;
+	public static void main(String args[])
+	{
+		int data=50;
+		System.out.println(data);
+		Varibable obj=new Varibable();
+		System.out.println(n);
+		System.out.println(obj.m);
+		
+	}
+
+}
